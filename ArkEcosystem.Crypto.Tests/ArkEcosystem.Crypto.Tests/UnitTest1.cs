2c312e1e4a61435af7deab1b0c46b2bf410a0bee
Brian Faust <hello@brianfaust.me>
2018-07-05 17:07:57 +0300

    chore: barebones tests solution

---
+++ ArkEcosystem.Crypto.Tests/ArkEcosystem.Crypto.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArkEcosystem.Crypto.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
