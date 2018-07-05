d98bcde2abd32c8f9b9f001a2734a9f5c84fa81f
Brian Faust <hello@brianfaust.me>
2018-07-05 16:03:27 +0300

    feat: transaction type enum

---
+++ ArkEcosystem.Crypto/ArkEcosystem.Crypto/Enums/TransactionTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace ArkEcosystem.Crypto.Enums
+{
+    public static class TransactionTypes
+    {
+        public static readonly byte TRANSFER = 0;
+        public static readonly byte SECOND_SIGNATURE_REGISTRATION = 1;
+        public static readonly byte DELEGATE_REGISTRATION = 2;
+        public static readonly byte VOTE = 3;
+        public static readonly byte MULTI_SIGNATURE_REGISTRATION = 4;
+        public static readonly byte IPFS = 5;
+        public static readonly byte TIMELOCK_TRANSFER = 6;
+        public static readonly byte MULTI_PAYMENT = 7;
+        public static readonly byte DELEGATE_RESIGNATION = 8;
+    }
+}
