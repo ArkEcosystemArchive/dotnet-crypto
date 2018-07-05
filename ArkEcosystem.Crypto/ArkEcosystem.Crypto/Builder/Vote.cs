4a5cba7d6cd5208b6a4eda5cd5d1480f48e73234
Brian Faust <hello@brianfaust.me>
2018-07-05 16:05:24 +0300

    feat: transaction builder - vote

---
+++ ArkEcosystem.Crypto/ArkEcosystem.Crypto/Builder/Vote.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace ArkEcosystem.Crypto.Builder
+{
+    public class Vote
+    {
+        public TransactionModel Create(List<string> votes, string secret, string secondSecret = null)
+        {
+            var transaction = new TransactionModel
+            {
+                Type = Enums.TransactionTypes.VOTE,
+                Fee = Enums.TransactionFees.VOTE
+            };
+            transaction.Asset.Add("votes", votes);
+            transaction.RecipientId = Identity.Address.FromSecret(secret);
+
+            return Builder.Sign(transaction, secret, secondSecret);
+        }
+    }
+}
