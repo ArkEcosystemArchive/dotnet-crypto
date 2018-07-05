580ab81e13dd5867845a16d4616ab7deb7809b8b
Brian Faust <hello@brianfaust.me>
2018-07-05 16:01:36 +0300

    feat: AIP11 serialising - multi signature registration

---
+++ ArkEcosystem.Crypto/ArkEcosystem.Crypto/Serialisers/MultiSignatureRegistration.cs
@@ -0,0 +1,29 @@
+using System.IO;
+
+namespace ArkEcosystem.Crypto.Serialisers
+{
+    public static class MultiSignatureRegistration
+    {
+        public static void Serialise(BinaryWriter writer, TransactionModel transaction)
+        {
+            var joined = "";
+
+            if (transaction.Version == 1)
+            {
+                foreach (var key in transaction.Asset["multisignature"]["keysgroup"])
+                {
+                    joined += key.Substring(1);
+                }
+            }
+            else
+            {
+                joined = string.Join(transaction.Asset["multisignature"]["keysgroup"]);
+            }
+
+            writer.Write((byte)transaction.Asset["multisignature"]["min"]);
+            writer.Write(transaction.Asset["multisignature"]["keysgroup"].Count);
+            writer.Write((byte)transaction.Asset["multisignature"]["lifetime"]);
+            writer.Write(joined);
+        }
+    }
+}
