ecc6e31569bf29142a86f9480d771974a97f36b1
Brian Faust <hello@brianfaust.me>
2018-07-05 16:01:50 +0300

    feat: AIP11 serialising - delegate resignation

---
+++ ArkEcosystem.Crypto/ArkEcosystem.Crypto/Serialisers/DelegateResignation.cs
@@ -0,0 +1,12 @@
+using System.IO;
+
+namespace ArkEcosystem.Crypto.Serialisers
+{
+    public static class DelegateResignation
+    {
+        public static void Serialise(BinaryWriter writer, TransactionModel transaction)
+        {
+            //
+        }
+    }
+}
