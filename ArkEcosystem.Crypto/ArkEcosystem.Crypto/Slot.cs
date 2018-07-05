c5229bbbf52a53c8cdd4ce376b38390875e905ef
Brian Faust <hello@brianfaust.me>
2018-07-05 16:00:50 +0300

    feat: slot time

---
+++ ArkEcosystem.Crypto/ArkEcosystem.Crypto/Slot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArkEcosystem.Crypto
+{
+    public static class Slot
+    {
+        public static uint GetTime()
+        {
+            return Convert.ToUInt32((DateTime.UtcNow - Configuration.Network.Get().GetEpoch()).TotalMilliseconds / 1000);
+        }
+    }
+}
