fe58177838282c0ba31637d95cc226e6c0d2471b
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-07-30 21:20:10 -0500

    Add explicit network tests to complete coverage

---
+++ ArkEcosystem.Crypto.Tests/Networks/DevnetTest.cs
@@ -0,0 +1,41 @@
+// Author:
+//       Brian Faust <brian@ark.io>
+//
+// Copyright (c) 2018 Ark Ecosystem <info@ark.io>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ArkEcosystem.Crypto.Tests.Configuration
+{
+    [TestClass]
+    public class DevnetTest
+    {
+        [TestMethod]
+        public void Should_Get_The_Devnet_values()
+        {
+            var devnet = new Crypto.Networks.Devnet();
+            Assert.AreEqual(new DateTime(2017, 3, 21, 13, 00, 0, DateTimeKind.Utc), devnet.GetEpoch());
+            Assert.AreEqual(0x1e, devnet.GetVersion());
+            Assert.AreEqual(170, devnet.GetWIF());
+        }
+    }
+}
